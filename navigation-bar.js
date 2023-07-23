4854f6d92075d1e024a9532904d7b7b8f4eb8d06
kw3705 <Kwu101020@gmail.com>
2023-07-23 17:11:08 -0400

    Nagivation JS Rename, Nav Bar Scaling Adjustments

--- navigation-bar.js
+++ navigation-bar.js
@@ -1,13 +1,13 @@
 function loadNavigationBar() {
-    const navigationBar = `
+  const navigationBar = `
       <div class="navigation-bar">
         <h1 class="logo">Kevin Wu</h1>
         <nav>
           <ul>
             <li><a href="#">About Me</a></li>
             <li><a href="#">Projects</a></li>
-            <li><a href="#">Github</a></li>
-            <li><a href="#">LinkedIn</a></li>
+            <li><a href="https://github.com/kwu1010">Github</a></li>
+            <li><a href="https://www.linkedin.com/in/kevin-wu-667a50266/">LinkedIn</a></li>
             <li><a href="#">Contact Me</a></li>
           </ul>
         </nav>
