c66f2974f07ace5f8963dd5994e1ee0dcec7da3a
kw3705 <Kwu101020@gmail.com>
2023-08-15 21:07:14 -0400

    Page styles updated & organized, resume embedded into about page

--- navigation/navigation-bar.js
+++ navigation/navigation-bar.js
@@ -4,11 +4,12 @@ function loadNavigationBar() {
             <h1 class="logo">Kevin Wu</h1>
             <nav>
                 <ul>
-                    <li><a href="#">About Me</a></li>
-                    <li><a href="#">Projects</a></li>
+                    <li><a href="/home/index.html">Home</a></li>
+                    <li><a href="/about/about_me.html">About Me</a></li>
+                    <li><a href="/404/404.html">Projects</a></li>
                     <li><a href="https://github.com/kwu1010">Github</a></li>
                     <li><a href="https://www.linkedin.com/in/kevin-wu-667a50266/">LinkedIn</a></li>
-                    <li><a href="#">Contact Me</a></li>
+                    <li><a href="/contact/contact_me.html">Contact Me</a></li>
                 </ul>
             </nav>
         </div>
