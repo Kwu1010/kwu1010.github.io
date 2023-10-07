6b0d7e7b11023085d3390e89631fe2812f7611a2
kw3705 <Kwu101020@gmail.com>
2023-10-07 01:05:20 -0400

    Added Loading Animation
    - index.html code moved to home.html
    - index.html replaced by gif & redirect
    - updated nav bar to home.html

--- navigation/navigation-bar.js
+++ navigation/navigation-bar.js
@@ -4,7 +4,7 @@ function loadNavigationBar() {
             <h1 class="logo">Kevin Wu</h1>
             <nav>
                 <ul>
-                    <li><a href="/index.html">Home</a></li>
+                    <li><a href="/home/home.html">Home</a></li>
                     <li><a href="/about/about_me.html">About Me</a></li>
                     <li><a href="/404/404.html">Projects</a></li>
                     <li><a href="https://github.com/kwu1010">Github</a></li>
